ad8173f9670423151136506bd2f5cdf2f45fd94d
luccaamp <luccaamp@gmail.com>
2025-09-26 22:11:48 -0300

    Commit 4: Moving method get_charge from Client to Rental

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 class Book:
-
     REGULAR: int = 0
     NEW_RELEASE: int = 1
     CHILDREN: int = 2
@@ -12,29 +11,28 @@ class Rental:
     def __init__(self, book: Book, days_rented: int):
         self.book = book
         self.days_rented = days_rented
+    
+    def get_charge(self) -> float:
+        amount = 0
+        if self.book.price_code == Book.REGULAR:
+            amount += 2
+            if self.days_rented > 2:
+                amount += (self.days_rented - 2) * 1.5
+        elif self.book.price_code == Book.NEW_RELEASE:
+            amount += self.days_rented * 3
+        elif self.book.price_code == Book.CHILDREN:
+            amount += 1.5
+            if self.days_rented > 3:
+                amount += (self.days_rented - 3) * 1.5
+        return amount
 
 class Client:
-
     def __init__(self, name: str):
         self.name = name
         self.rentals = []
 
     def add_rental(self, rental: Rental):
         self.rentals.append(rental)
-    
-    def get_charge(self, rental: Rental) -> float:
-        amount = 0
-        if rental.book.price_code == Book.REGULAR:
-            amount += 2
-            if rental.days_rented > 2:
-                amount += (rental.days_rented - 2) * 1.5
-        elif rental.book.price_code == Book.NEW_RELEASE:
-            amount += rental.days_rented * 3
-        elif rental.book.price_code == Book.CHILDREN:
-            amount += 1.5
-            if rental.days_rented > 3:
-                amount += (rental.days_rented - 3) * 1.5
-        return amount
 
     def statement(self) -> str:
         total_amount = 0
@@ -42,7 +40,8 @@ class Client:
         result = f"Rental summary for {self.name}\n"
 
         for rental in self.rentals:
-            amount = self.get_charge(rental)
+            # usar o método movido para Rental
+            amount = rental.get_charge()
 
             # add frequent renter points
             frequent_renter_points += 1
